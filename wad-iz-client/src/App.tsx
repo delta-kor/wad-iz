43406d973780f099ac1a295fc488bc937fd72604
delta-kor <deltakbot1@gmail.com>
2021-05-09 18:17:21 +0900

    Add profile update

--- wad-iz-client/src/App.tsx
+++ wad-iz-client/src/App.tsx
@@ -60,6 +60,12 @@ const PcProfileWrapper = styled(motion.div)`
   top: calc(50% - 276px / 2);
 `;
 
+interface User {
+  userId: string;
+  nickname: string;
+  profileImage: string;
+}
+
 interface State {
   menu: number;
   directAmount: number;
@@ -68,10 +74,13 @@ interface State {
   wadizSupporter: number;
   dailyUp: number;
   dailyDown: number;
+
+  userId: string;
+  users: User[];
 }
 
 export default class App extends Component<any, State> {
-  private socket: Socket = new Socket();
+  private socket!: Socket;
   public profileImageMap: Map<string, string> = new Map();
 
   constructor(props: any) {
@@ -84,13 +93,54 @@ export default class App extends Component<any, State> {
       wadizSupporter: 0,
       dailyUp: 1,
       dailyDown: 1,
+      userId: '',
+      users: [],
     };
   }
 
   componentDidMount() {
+    this.socket = new Socket();
+
     this.socket.on('multiple-connect', () => {
       alert('다른 기기에서 접속하여 서버와의 연결을 끊었습니다');
     });
+
+    this.socket.on('ticket', (packet: TicketServerPacket) => {
+      this.setState({ userId: packet.user_id });
+      const users = this.state.users;
+      users.push({
+        userId: packet.user_id,
+        nickname: packet.nickname,
+        profileImage: packet.profile_image,
+      });
+      this.setState({ users });
+    });
+
+    this.socket.on('connect', (packet: ConnectServerPacket) => {
+      const users = this.state.users.filter(user => user.userId !== packet.user_id);
+      users.push({
+        userId: packet.user_id,
+        nickname: packet.nickname,
+        profileImage: packet.profile_image,
+      });
+      this.setState({ users });
+    });
+    this.socket.on('disconnect', (packet: DisconnectServerPacket) => {
+      const users = this.state.users.filter(user => user.userId !== packet.user_id);
+      this.setState({ users });
+    });
+    this.socket.on('user-sync', (packet: UserSyncServerPacket) => {
+      const users = this.state.users;
+      for (const user of packet.users) {
+        users.push({
+          userId: user.user_id,
+          nickname: user.nickname,
+          profileImage: user.profile_image,
+        });
+      }
+      this.setState({ users });
+    });
+
     this.socket.on('token', (packet: TokenServerPacket) => {
       localStorage.setItem('token', packet.token);
     });
@@ -109,15 +159,47 @@ export default class App extends Component<any, State> {
     this.socket.on('daily-update', (packet: DailyUpdateServerPacket) => {
       this.setState({ dailyUp: packet.up, dailyDown: packet.down });
     });
+
     this.socket.on('profile-image', (packet: ProfileImageServerPacket) => {
       for (const image of packet.images) {
         this.profileImageMap.set(image.key, image.url);
       }
     });
+
+    this.socket.on('profile-update', (packet: ProfileUpdateServerPacket) => {
+      const users = this.state.users.filter(user => user.userId !== packet.user_id);
+      users.push({
+        userId: packet.user_id,
+        nickname: packet.nickname,
+        profileImage: packet.profile_image,
+      });
+      this.setState({ users });
+    });
   }
+
+  getMyProfile = (): User => {
+    for (const user of this.state.users) {
+      if (user.userId === this.state.userId) return user;
+    }
+    return { userId: this.state.userId, nickname: '', profileImage: '' };
+  };
 
   onNavigatorClick = (index: number) => {
     if (index === 0 || index === 2) this.setState({ menu: index });
+  };
+
+  onProfileInteract = (type: 'left' | 'right' | 'nickname') => {
+    if (type === 'nickname') {
+      const nickname = prompt('닉네임을 입력하세요');
+      if (!nickname || !nickname.trim().length) {
+        return false;
+      }
+      if (nickname.length > 12) {
+        alert('닉네임은 12자 이내여야 합니다');
+        return false;
+      }
+      this.socket.updateProfile(nickname, this.socket.profileImage!);
+    }
   };
 
   render() {
@@ -178,19 +260,22 @@ export default class App extends Component<any, State> {
         </motion.div>
       );
     } else if (this.state.menu === 2) {
+      const profile = this.getMyProfile();
       content = (
         <ProfileWrapper initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
           <Profile
-            nickname={this.socket.nickname!}
-            profileImageUrl={this.profileImageMap.get(this.socket.profileImage!)}
+            nickname={profile.nickname}
+            profileImageUrl={this.profileImageMap.get(profile.profileImage)}
+            onInteract={this.onProfileInteract}
           />
         </ProfileWrapper>
       );
       pcContent = (
         <PcProfileWrapper initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
           <Profile
-            nickname={this.socket.nickname!}
-            profileImageUrl={this.profileImageMap.get(this.socket.profileImage!)}
+            nickname={profile.nickname}
+            profileImageUrl={this.profileImageMap.get(profile.profileImage)}
+            onInteract={this.onProfileInteract}
           />
         </PcProfileWrapper>
       );
